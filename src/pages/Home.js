f3ccf6988929674e9454938206cee46fffc73ac4
prashantmishra-ai <thisisprashantmishra@gmail.com>
2022-08-07 17:17:32 +0530

    added Actors search

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,43 +1,78 @@
 import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
-import {apiGet} from "../misc/config";
+import { apiGet } from '../misc/config';
 const Home = () => {
-  const [input, setInput] = useState("")
-  const [results, setResults] = useState(null)
-  const onInputChange = (ev) =>{
-    setInput(ev.target.value)
-  }
-  const onSearch = () =>{
-    apiGet(`/search/shows?q=${input}`).then(result=>{
-      setResults(result)
-     })
-  }
-  const onKeyDown = (ev) =>{
-    if(ev.keyCode === 13){
-      onSearch()
+  const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
+  const [searchOption, setsearchOption] = useState('shows');
+  const isShowsSearch = searchOption === 'shows';
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+  };
+  const onSearch = () => {
+    apiGet(`/search/${searchOption}?q=${input}`).then(result => {
+      setResults(result);
+    });
+  };
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      onSearch();
     }
-  }
-  const renderResults = () =>{
-    if (results && results.length === 0){
-        return <div>No Match Found for the Input Provided</div>
+  };
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No Match Found for the Input Provided</div>;
     }
 
-    if (results && results.length > 0){
-      return <div>
-        {
-          results.map((item)=><div key={item.show.id}>{item.show.name}</div>)
-        }
-      </div>
+    if (results && results.length > 0) {
+      return results[0].show
+        ? results.map(item => <div key={item.show.id}>{item.show.name}</div>)
+        : results.map(item => (
+            <div key={item.person.id}>{item.person.name}</div>
+          ));
     }
     return null;
-  }
+  };
+  const onRadioChange = ev => {
+    setsearchOption(ev.target.value);
+  };
   return (
     <MainPageLayout>
-    <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input}/>
-    <button type='button' onClick={onSearch}>Search</button>
-    {
-      renderResults()
-    }
+      <input
+        type="text"
+        onChange={onInputChange}
+        placeholder="Search for Something"
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      <div>
+        <label htmlFor="show-search">
+          {' '}
+          Shows
+          <input
+            type="radio"
+            id="show-search"
+            value="shows"
+            checked={isShowsSearch}
+            onChange={onRadioChange}
+          />
+        </label>
+        <label htmlFor="actors-search">
+          {' '}
+          Actors
+          <input
+            type="radio"
+            id="actors-search"
+            value="people"
+            checked={!isShowsSearch}
+            onChange={onRadioChange}
+          />
+        </label>
+      </div>
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+      {renderResults()}
     </MainPageLayout>
   );
 };
